c94ab6af842203c0cc12cd1dcdfb19caac9e7305
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2024-07-23 21:50:33 +0200

    Added a few more functions

--- iterby.go
+++ iterby.go
@@ -19,3 +19,60 @@ func Enumerate[T any](f func(func(T) bool)) func(func(int, T) bool) {
 		}
 	}
 }
+
+func Count() func(func(int) bool) {
+	return func(yield func(int) bool) {
+		var index int
+		for {
+			if !yield(index) {
+				return
+			}
+			index++
+		}
+	}
+}
+
+func Chain[T any](args ...[]T) func(func(T) bool) {
+	return func(yield func(T) bool) {
+		for _, s := range args {
+			for _, t := range s {
+				if !yield(t) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func Cycle[T any](args ...[]T) func(func(T) bool) {
+	return func(yield func(T) bool) {
+		for {
+			for t := range Chain(args...) {
+				if !yield(t) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func RangeFilter[T any](begin func(T) bool, end func(T) bool, f func(func(T) bool)) func(func(T) bool) {
+	return func(yield func(T) bool) {
+		var inRange bool
+		for t := range f {
+			if !inRange {
+				if begin(t) {
+					inRange = true
+				} else {
+					continue
+				}
+			}
+			if !yield(t) {
+				return
+			}
+			if end(t) {
+				inRange = false
+			}
+		}
+	}
+}
